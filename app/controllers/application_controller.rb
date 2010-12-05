ca9422c2dbcf5a35de26234e57dc1874b8e9e9b0
Markus Knittig <markus@myd0.de>
2010-12-05 22:12:27 +0100

    Don't log sensitive data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Scrub sensitive parameters from your log
+  filter_parameter_logging :password, :password_confirmation
 end
