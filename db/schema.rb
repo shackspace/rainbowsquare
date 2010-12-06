cc18b7a114c367bf73c98f6338a160973f91b807
Markus Knittig <markus@myd0.de>
2010-12-06 14:53:52 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101205203021) do
+ActiveRecord::Schema.define(:version => 20101205205901) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                             :default => "", :null => false
+    t.string   "encrypted_password", :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                     :default => "", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "entries", :force => true do |t|
     t.string   "name"
